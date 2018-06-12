ed39065437e528f965a59c49cb7bdc12e5bcc5d8
Wayou Liu <liuwayong@gmail.com>
2018-06-12 15:17:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-$('p').click(function(){
-	$('#wrap').css({
-		'overflow-y': 'hidden',
-		'-webkit-overflow-scrolling': 'auto'
-	});
+// $('p').click(function(){
+// 	$('#wrap').css({
+// 		'overflow-y': 'hidden',
+// 		'-webkit-overflow-scrolling': 'auto'
+// 	});
 
-	$('#overlay').css({
-		display:'block'
-	})
-})
+// 	$('#overlay').css({
+// 		display:'block'
+// 	})
+// })
 
-$('#overlay').click(function(){
-	$('#wrap').css({
-		'overflow-y': 'scroll',
-		'-webkit-overflow-scrolling': 'touch'
-	});
+// $('#overlay').click(function(){
+// 	$('#wrap').css({
+// 		'overflow-y': 'scroll',
+// 		'-webkit-overflow-scrolling': 'touch'
+// 	});
 
-	$('#overlay').css({
-		display:'none'
-	})
-})
+// 	$('#overlay').css({
+// 		display:'none'
+// 	})
+// })
